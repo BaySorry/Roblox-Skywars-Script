0421a925ad67b01d92e20d90b6ee21d44515a4f6
Axolotl <76564648+BaySorry@users.noreply.github.com>
2022-12-04 19:43:46 +0300

    Update Skywars.lua

--- Skywars.lua
+++ Skywars.lua
@@ -500,7 +500,7 @@ local Credits = page:addSection("Credits - Also credits to MoRnD#7976 for these 
 
 
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/BaySorry/Roblox-Skywars-Script/main/Xray.lua'),true))()
+
 
 -- load
 venyx:SelectPage(venyx.pages[1], true)
